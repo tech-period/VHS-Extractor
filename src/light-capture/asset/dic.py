09d1496442c132dfad18f30bc627c5a6c4206b75
KINGE <g.otera.development@gmail.com>
2021-12-03 22:10:39 +0900

    保存先変更メソッドを作成

--- src/light-capture/asset/dic.py
+++ src/light-capture/asset/dic.py
@@ -4,6 +4,9 @@ dic = {
     "rec" : asset_path + "rec_button.png",
     "stop" : asset_path + "stop_button.png",
     "setting" : asset_path + "setting_button.png",
+    "destination" : asset_path + "destination.png",
+    "ok" : asset_path + "ok_button.png",
+    "cancel" : asset_path + "cancel_button.png",
     "trashbox" : asset_path + "trashbox.png",
 }
 
--- src/light-capture/lc_control.py
+++ src/light-capture/lc_control.py
@@ -1,34 +1,53 @@
 import pyautogui as gui
+import time
 from asset import dic
 
 class lc_control():
+
     # 録画開始
     def start_rec():
         try:
             x,y = gui.locateCenterOnScreen(dic.get("rec"))
             gui.click(x,y)
         except Exception as e:
-            find_nothing(e)
+            lc_control.find_nothing(e)
 
     # 録画終了
     def stop_rec():
         try:
             x,y = gui.locateCenterOnScreen(dic.get("stop"))
             gui.click(x,y)
-        except gui.raisePyAutoGUIImageNotFoundException as e:
-            print()
+        # except gui.raisePyAutoGUIImageNotFoundException as e:
+        #     print()
         except Exception as e:
-            find_nothing(e)
+            lc_control.find_nothing(e)
 
     # 設定を開く
     def open_settings():
         try:
             x,y = gui.locateCenterOnScreen(dic.get("setting"))
             gui.click(x,y)
-        except gui.raisePyAutoGUIImageNotFoundException as e:
-            print()
+            # ダイアログが開くため、1秒待機
+            time.sleep(1)
         except Exception as e:
-            find_nothing(e)
+            lc_control.find_nothing(e)
+
+    # 保存先を変更
+    def change_destination(destination):
+        try:
+            # print(destination)
+            x,y = gui.locateCenterOnScreen(dic.get("destination"))
+            gui.click(x+200,y)
+            gui.hotkey('ctrl', 'a')
+            gui.write(destination)
+
+            # debug時はキャンセルボタンを押す
+            # x,y = gui.locateCenterOnScreen(dic.get("ok"))
+            x,y = gui.locateCenterOnScreen(dic.get("cancel"))
+            # gui.click(x,y)
+            
+        except Exception as e:
+            lc_control.find_nothing(e)
 
     # ボタン検出失敗時
     def find_nothing(e):
@@ -36,7 +55,10 @@ class lc_control():
         print(e)
 
 
+
 # debug用
 lc_control.open_settings()
+str = str("C"+["shift", ":"]+"\\Users\\goter\\Videos\\Captures")
+lc_control.change_destination(str)
 # lc_control.start_rec()
 # lc_control.stop_rec()
