9c598e6d56eb7bebb9aa0815602fce3c861f1a7b
KINGE <g.otera.development@gmail.com>
2021-12-03 21:24:59 +0900

    設定を開くメソッドを追加

--- src/light-capture/asset/dic.py
+++ src/light-capture/asset/dic.py
@@ -3,6 +3,7 @@ asset_path = "./src/light-capture/asset/"
 dic = {
     "rec" : asset_path + "rec_button.png",
     "stop" : asset_path + "stop_button.png",
+    "setting" : asset_path + "setting_button.png",
     "trashbox" : asset_path + "trashbox.png",
 }
 
--- src/light-capture/lc_control.py
+++ src/light-capture/lc_control.py
@@ -1,16 +1,42 @@
 import pyautogui as gui
 from asset import dic
 
-# path = "./src/light-capture/asset/rec_button.png"
-
 class lc_control():
+    # 録画開始
     def start_rec():
         try:
-            x,y = gui.locateCenterOnScreen(dic.get("trashbox"))
-            gui.doubleClick(x,y)
+            x,y = gui.locateCenterOnScreen(dic.get("rec"))
+            gui.click(x,y)
         except Exception as e:
-            print("対象が見つかりませんでした。")
-            print(e)
+            find_nothing(e)
+
+    # 録画終了
+    def stop_rec():
+        try:
+            x,y = gui.locateCenterOnScreen(dic.get("stop"))
+            gui.click(x,y)
+        except gui.raisePyAutoGUIImageNotFoundException as e:
+            print()
+        except Exception as e:
+            find_nothing(e)
+
+    # 設定を開く
+    def open_settings():
+        try:
+            x,y = gui.locateCenterOnScreen(dic.get("setting"))
+            gui.click(x,y)
+        except gui.raisePyAutoGUIImageNotFoundException as e:
+            print()
+        except Exception as e:
+            find_nothing(e)
+
+    # ボタン検出失敗時
+    def find_nothing(e):
+        print("対象が見つかりませんでした")
+        print(e)
 
 
-lc_control.start_rec()
+# debug用
+lc_control.open_settings()
+# lc_control.start_rec()
+# lc_control.stop_rec()
