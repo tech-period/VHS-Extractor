b6962d4a753e01c1d3442ad9a9a9a79184b9da11
KINGE <g.otera.development@gmail.com>
2021-12-07 03:20:04 +0900

    API通信用サービスを作成

---
+++ src/switchbot/service.py
@@ -0,0 +1,17 @@
+import requests
+
+from asset import token
+
+class service():
+
+    BODY_BASE = "https://api.switch-bot.com/v1.0/devices"
+    HEADER = token.token_id
+
+    response = any
+
+    def __init__():
+        service.response = requests.get(service.BODY_BASE, headers=service.HEADER)
+        
+
+
+        
