c6be2ed3768dc74fd90507a951432eeb798df7c2
KINGE <g.otera.development@gmail.com>
2021-12-07 12:16:12 +0900

    サンプルを参考にAPI通信を実装
    https://note.com/klayer123/n/nb14086840351

--- src/switchbot/service.py
+++ src/switchbot/service.py
@@ -1,16 +1,26 @@
+import json
 import requests
 
-from asset import token
+from asset import token as t
 
 class service():
+    def __init__(self):
+        self.body_base = "https://api.switch-bot.com/v1.0/devices"
+        self.header = {"Authorization" : t.token_id}
+        self.response = requests.get(self.body_base, headers=self.header)        
+        self.devices  = json.loads(self.response.text)
 
-    BODY_BASE = "https://api.switch-bot.com/v1.0/devices"
-    HEADER = {"Authorization" : token.token_id}
+        # Get switchbot bot "deviceId" in all device information
+        self.bots_id  = [device["deviceId"] for device in self.devices['body']['deviceList']]
 
-    response = any
+this = service()
 
-    def __init__():
-        service.response = requests.get(service.BODY_BASE, headers=service.HEADER)
-        
+# Get all switchbot bot power state and output on your display
+for bot_id in this.bots_id:
+ 
+    response = requests.get("https://api.switch-bot.com/v1.0/devices/" + bot_id + "/status", headers=this.header)
+    bot      = json.loads(response.text)
+
+    print("bot id : " + bot_id)
 
 
