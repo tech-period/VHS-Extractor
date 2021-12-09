6d95fe62acddaac8e23b65ce3c6613e048729e80
KINGE <g.otera.development@gmail.com>
2021-12-10 00:56:38 +0900

    デバイス名が"レコーダー"のIDのみを取得するようリファクタリング

--- src/switchbot/service.py
+++ src/switchbot/service.py
@@ -13,32 +13,33 @@ class service():
         self.response = requests.get(self.body_base, headers=self.header)        
         self.devices  = json.loads(self.response.text)
 
-        # Get switchbot bot "deviceId" in all device information
-        # self.bots_id  = [device["deviceId"] for device in self.devices['body']['deviceList']]
-        self.bots_id  = [device["deviceId"] for device in self.devices['body']['infraredRemoteList']]
+        # Get "deviceId"
+        self.bot_id  = [device["deviceId"] for device in self.devices['body']['infraredRemoteList'] if "レコーダー" == device['deviceName']]
 
         print(self.response.text)
 
 
+def active_test():
+    this = service()
+    cmd = commands.commands()
 
-this = service()
-cmd = commands.commands()
+    url = this.body_base + "/" + this.bot_id[0] + "/commands"
+    print("url : " + url)
 
-url = this.body_base + "/" + this.bots_id[0] + "/commands"
-print("url : " + url)
+    print("body_home")
+    result = requests.post(url, headers=this.post_header, data=json.dumps(cmd.home))
+    time.sleep(3)
 
-print("body_home")
-result = requests.post(url, headers=this.post_header, data=json.dumps(cmd.home))
-time.sleep(3)
+    print("body_up")
+    result = requests.post(url, headers=this.post_header, data=json.dumps(cmd.up))
+    time.sleep(1)
 
-print("body_up")
-result = requests.post(url, headers=this.post_header, data=json.dumps(cmd.up))
-time.sleep(1)
+    print("body_down")
+    result = requests.post(url, headers=this.post_header, data=json.dumps(cmd.down))
+    time.sleep(1)
 
-print("body_down")
-result = requests.post(url, headers=this.post_header, data=json.dumps(cmd.down))
-time.sleep(1)
+    print("body_enter")
+    result = requests.post(url, headers=this.post_header, data=json.dumps(cmd.enter))
+    time.sleep(1)
 
-print("body_enter")
-result = requests.post(url, headers=this.post_header, data=json.dumps(cmd.enter))
-time.sleep(1)
+active_test()
