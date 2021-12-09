4b308c5545fd49c3412a83cc68c26d9531dd3cbe
KINGE <g.otera.development@gmail.com>
2021-12-10 01:45:46 +0900

    コマンド実行処理を抽象化（リファクタリング）

--- src/switchbot/commands.py
+++ src/switchbot/commands.py
@@ -1,35 +1,37 @@
 class commands():
 
-    # 基本コマンド
-    home = {
-        "command": "ホーム",
-        "parameter": "default",
-        "commandType": "customzie"
-    }
-    enter = {
-        "command": "決定",
-        "parameter": "default",
-        "commandType": "customzie"
-    }
+    command_list = {
+        # 基本コマンド
+        "home" : {
+            "command": "ホーム",
+            "parameter": "default",
+            "commandType": "customzie"
+        },
+        "enter" : {
+            "command": "決定",
+            "parameter": "default",
+            "commandType": "customzie"
+        },
 
-    # 矢印コマンド
-    up = {
-        "command": "上",
-        "parameter": "default",
-        "commandType": "customzie"
-    }
-    down = {
-        "command": "下",
-        "parameter": "default",
-        "commandType": "customzie"
-    }
-    right = {
-        "command": "右",
-        "parameter": "default",
-        "commandType": "customzie"
-    }
-    left = {
-        "command": "左",
-        "parameter": "default",
-        "commandType": "customzie"
+        # 矢印コマンド
+        "up" : {
+            "command": "上",
+            "parameter": "default",
+            "commandType": "customzie"
+        },
+        "down" : {
+            "command": "下",
+            "parameter": "default",
+            "commandType": "customzie"
+        },
+        "right" : {
+            "command": "右",
+            "parameter": "default",
+            "commandType": "customzie"
+        },
+        "left" : {
+            "command": "左",
+            "parameter": "default",
+            "commandType": "customzie"
+        },
     }
--- src/switchbot/service.py
+++ src/switchbot/service.py
@@ -7,39 +7,28 @@ import commands
 
 class service():
     def __init__(self):
-        self.body_base = "https://api.switch-bot.com/v1.0/devices"
-        self.header = {"Authorization" : t.token_id}
-        self.post_header = { 'Content-Type': 'application/json; charset: utf8', "Authorization" : t.token_id }
-        self.response = requests.get(self.body_base, headers=self.header)        
-        self.devices  = json.loads(self.response.text)
+        self.__body_base = "https://api.switch-bot.com/v1.0/devices"
+        self.__header = {"Authorization" : t.token_id}
+        self.__post_header = { 'Content-Type': 'application/json; charset: utf8', "Authorization" : t.token_id }
+        self.__response = requests.get(self.__body_base, headers=self.__header)
+        self.devices  = json.loads(self.__response.text)
 
         # Get "deviceId"
         self.bot_id  = [device["deviceId"] for device in self.devices['body']['infraredRemoteList'] if "レコーダー" == device['deviceName']]
 
-        print(self.response.text)
+        print(self.__response.text)
 
+    def execute_command(self, command_name:str, wait_time:int = 0):
+        cmd = commands.commands()
+        url = self.__body_base + "/" + self.bot_id[0] + "/commands"
+        # コマンドをAPIサーバーにPOSTして指定時間待機する
+        requests.post(url, headers=self.__post_header, data=json.dumps(cmd.command_list[command_name]))
+        time.sleep(wait_time)
+        print("The \"" + command_name + "\" command was executed, and wait " + str(wait_time) + "second")
 
-def active_test():
-    this = service()
-    cmd = commands.commands()
-
-    url = this.body_base + "/" + this.bot_id[0] + "/commands"
-    print("url : " + url)
-
-    print("body_home")
-    result = requests.post(url, headers=this.post_header, data=json.dumps(cmd.home))
-    time.sleep(3)
-
-    print("body_up")
-    result = requests.post(url, headers=this.post_header, data=json.dumps(cmd.up))
-    time.sleep(1)
-
-    print("body_down")
-    result = requests.post(url, headers=this.post_header, data=json.dumps(cmd.down))
-    time.sleep(1)
-
-    print("body_enter")
-    result = requests.post(url, headers=this.post_header, data=json.dumps(cmd.enter))
-    time.sleep(1)
-
-active_test()
+# 動作テスト
+service = service()
+service.execute_command("home", 3)
+service.execute_command("up", 1)
+service.execute_command("down", 1)
+service.execute_command("enter", 1)
