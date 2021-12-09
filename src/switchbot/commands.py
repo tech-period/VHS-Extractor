8438803685cc55ffe6318102148a44e942ef3e5e
KINGE <g.otera.development@gmail.com>
2021-12-10 00:34:45 +0900

    コマンドの定義を別ファイルに転記

---
+++ src/switchbot/commands.py
@@ -0,0 +1,35 @@
+class commands():
+
+    # 基本コマンド
+    home = {
+        "command": "ホーム",
+        "parameter": "default",
+        "commandType": "customzie"
+    }
+    enter = {
+        "command": "決定",
+        "parameter": "default",
+        "commandType": "customzie"
+    }
+
+    # 矢印コマンド
+    up = {
+        "command": "上",
+        "parameter": "default",
+        "commandType": "customzie"
+    }
+    down = {
+        "command": "下",
+        "parameter": "default",
+        "commandType": "customzie"
+    }
+    right = {
+        "command": "右",
+        "parameter": "default",
+        "commandType": "customzie"
+    }
+    left = {
+        "command": "左",
+        "parameter": "default",
+        "commandType": "customzie"
+    }
--- src/switchbot/service.py
+++ src/switchbot/service.py
@@ -3,6 +3,7 @@ import requests
 import time
 
 from asset import token as t
+import commands
 
 class service():
     def __init__(self):
@@ -18,52 +19,26 @@ class service():
 
         print(self.response.text)
 
+
+
 this = service()
+cmd = commands.commands()
 
-# Get all switchbot bot power state and output on your display
-for bot_id in this.bots_id:
- 
-    # create API body 
-    # suport cool state 
-    body_home = {
-        "command": "ホーム",
-        "parameter": "default",
-        "commandType": "customzie"
-    }
-    body_left = {
-        "command": "左",
-        "parameter": "default",
-        "commandType": "customzie"
-    }
-    body_right = {
-        "command": "右",
-        "parameter": "default",
-        "commandType": "customzie"
-    }
-    body_up = {
-        "command": "上",
-        "parameter": "default",
-        "commandType": "customzie"
-    }
-    body_down = {
-        "command": "下",
-        "parameter": "default",
-        "commandType": "customzie"
-    }
-    body_enter = {
-        "command": "決定",
-        "parameter": "default",
-        "commandType": "customzie"
-    }
+url = this.body_base + "/" + this.bots_id[0] + "/commands"
+print("url : " + url)
 
-    url = this.body_base + "/" + bot_id + "/commands"
-    print("url : " + url)
-    print("body_up")
-    result = requests.post(url, headers=this.post_header, data=json.dumps(body_up))
-    time.sleep(1)
-    print("body_down")
-    result = requests.post(url, headers=this.post_header, data=json.dumps(body_down))
-    time.sleep(1)
-    print("body_enter")
-    result = requests.post(url, headers=this.post_header, data=json.dumps(body_enter))
-    time.sleep(1)
+print("body_home")
+result = requests.post(url, headers=this.post_header, data=json.dumps(cmd.home))
+time.sleep(3)
+
+print("body_up")
+result = requests.post(url, headers=this.post_header, data=json.dumps(cmd.up))
+time.sleep(1)
+
+print("body_down")
+result = requests.post(url, headers=this.post_header, data=json.dumps(cmd.down))
+time.sleep(1)
+
+print("body_enter")
+result = requests.post(url, headers=this.post_header, data=json.dumps(cmd.enter))
+time.sleep(1)
