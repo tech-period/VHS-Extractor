1bf02d4ae2aad3e6a45f1bc934432ebf246fe3ae
KINGE <g.otera.development@gmail.com>
2021-12-22 22:40:15 +0900

    エンティティのデータ構造を整形

--- src/core/entity.py
+++ src/core/entity.py
@@ -3,14 +3,22 @@ import shelve
 class entity():
     def __init__(self) -> None:
         self.entity = shelve.open('entity')
-        # self.entity['data'] = {'drive' : 'C:', 'path': '%USERPROFILE%'}
+        if self.entity['data'] == None:
+            self.entity['data'] = {
+                'drive' : 'C:',
+                'conditions': [
+                    {'type':'8mm, miniDV','check':True,},
+                    {'type':'VHS','check':False,}
+                ]
+            }
         print("前回条件")
         print(self.entity['data']['drive'])
         print(self.entity['data']['path'])
         # self.path = self.entity_data['path']
         # self.check_condition = self.entity_data['check_condition']
     
-    def get(self, key):
+    def get(self, key:str) -> str:
+        result = str()
         return self.entity['data'][key]
 
     def set(self, key, data) -> None:
